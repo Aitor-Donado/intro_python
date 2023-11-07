450ec56a86d6770f83f80494cba6cab9c3ab660c
Aitor <aitordonado@gmail.com>
2023-11-07 12:53:05 +0100

    Añadir ejercicios Funciones

---
+++ 05 Funciones (Ejercicios).py
@@ -0,0 +1,19 @@
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# 1. Crear una función que divida un número por tres.
+
+
+
+# 2. Crear una función que eleve a la 10 un determinado valor.
+
+
+
+# 3. Crear una función que calcule el área de una circunferencia.
+
+
+
+# 4. Crear una función que calcule el área de un triángulo.
