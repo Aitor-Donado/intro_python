ecb70d8432be37ef82903fe1130f03dab594b9b5
Aitor <aitordonado@gmail.com>
2023-11-07 12:50:25 +0100

    Incorporación de ejercicios

---
+++ 02_Listas y otros agregados (Ejercicios).py
@@ -0,0 +1,14 @@
+# 1- Crear una lista con los meses del año (lista1).
+
+
+# 2- Crear un alias de la lista (lista2).
+
+
+# 3- Clonar la lista (lista3).
+
+
+# 4- Añadir a la lista1 “Fin de Año”.
+
+
+# 5- Mostrar la lista2 y la lista3.
+
